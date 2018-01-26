28ada9db3df5554820206e6871c6d52694d71728
jpwebdev <jjpwebdev@gmail.com>
2018-01-26 10:58:38 -0500

    comments added, routes folder created, routes required, pug templating engine setup

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+// Requiring Modules
 const express = require('express');
 const bodyParser = require('body-parser');
+const mainRoutes = require('./routes');
 
+// Init app
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Middleware: BodyParse
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(bodyParser.json());
 
+// Middleware: Public Folder
 app.use(express.static("app/public"));
 
+// Handling Routes
+app.use("/", mainRoutes);
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
+// Start Server
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
