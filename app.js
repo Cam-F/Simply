3f61e91e9f65cfbd26683e7752cf9fbdb9248101
cameron fuller <cameronmfuller@yahoo.com>
2018-01-26 10:40:05 -0500

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.use(express.static("app/public"));
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
