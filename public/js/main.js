ff9cc80e70935afcae446afcb6546b705fa693c0
jpwebdev <jjpwebdev@gmail.com>
2018-01-27 13:46:00 -0500

    Overlay functionality working: login, signup, createEvent, joinEvent are displayed in homepage without navigating to a different page

--- public/js/main.js
+++ public/js/main.js
@@ -15,19 +15,28 @@
                 this.$nav_joinEvent_btn = $('#nav-joinEvent');
             },
             eventBinding () {
-                this.$nav_register_btn.on('click', this.overlayOpen);
-                this.$nav_signin_btn.on('click', this.overlayOpen);
-                this.$nav_createEvent_btn.on('click', this.overlayOpen);
-                this.$nav_joinEvent_btn.on('click', this.overlayOpen);
+                this.$nav_register_btn.on('click', this.overlayOpen.bind(this));
+                this.$nav_signin_btn.on('click', this.overlayOpen.bind(this));
+                this.$nav_createEvent_btn.on('click', this.overlayOpen.bind(this));
+                this.$nav_joinEvent_btn.on('click', this.overlayOpen.bind(this));
+                this.$overlay.on('click', this.closeOverlay);
             },
-            overlayOpen () {
-                console.log($(this));
-                const $section = $(this).attr('data-section');
-                simply.$overlay.addClass('open');
+            overlayOpen (e) {   // Function to open the overlay and display signup, login, createevent, joinevent sections             
+                // Remove previous section if there's one
+                this.closeOverlay();
+                // Get the name of the section we want to display
+                const $section = $(e.target).attr('data-section');
                 console.log($section);
+                // Add .open to the elements that we want to display
+                this.$overlay.addClass('open');
                 $(`.${$section}`).addClass('open');
 
+            },
+            closeOverlay () {   // Close any existing overlay
+                // Remove previous section if there's one
+                $('.open').removeClass('open');
             }
+            
         }
 
         simply.init();
